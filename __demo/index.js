0676340a25b9527c129282b9df595877425e2661
luo0412 <1095847440@qq.com>
2022-05-03 16:03:30 +0000

    adjust

--- __demo/index.js
+++ __demo/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import styles from './index.less';
+
+import ReactMarkdown from 'react-markdown';
+import remarkDirective from 'remark-directive';
+import remarkDirectiveRehype from 'remark-directive-rehype';
+
+const markdown = `
+# Cat videos
+
+::youtube-video[Video of a cat in a box]{#01ab2cd3efg}
+`;
+
+const YouTubeVideo = ({ id, children }) => (
+  <iframe src={'https://www.youtube.com/embed/' + id} width="200" height="200">
+    {children}
+  </iframe>
+);
 
 const App = () => {
   return (
     <div>
-      <h1 className={styles.title}>首页</h1>
+      <ReactMarkdown
+        children={markdown}
+        remarkPlugins={[remarkDirective, remarkDirectiveRehype]}
+        components={{
+          'youtube-video': YouTubeVideo,
+        }}
+      />
     </div>
   );
 };
